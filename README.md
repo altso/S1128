194346e43d7b725fbe7f09737a5bcb5bd7ab4a1a
Aliaksandr Tsoi <aliaksandr.tsoi@aqr.com>
2019-10-22 15:00:54 -0400

    Add sample project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # S1128
-A sample repo to demonstrate false positive on S1128 in SonarAnalyzer.CSharp 8.0.0.9566
+A sample repo to demonstrate false positive on S1128 in `SonarAnalyzer.CSharp` `8.0.0.9566`
---
+++ Sample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using TaskTupleAwaiter;
+
+namespace Sample
+{
+    public class SampleClass
+    {
+        public async Task SampleMethod()
+        {
+            (string str, Guid guid) = await (GetStringAsync(), GetGuidAsync());
+        }
+
+        private static Task<string> GetStringAsync() => Task.FromResult("Sample");
+
+        private static Task<Guid> GetGuidAsync() => Task.FromResult(Guid.NewGuid());
+    }
+}
